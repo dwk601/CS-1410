e0c51e24fa96d9fff77a2e97496dd263ee3cfd9c
dwk601 <10829539@uvu.edu>
2022-09-15 21:01:06 -0600

    finished Dessert shop part 3

--- dessertshop.py
+++ dessertshop.py
@@ -8,6 +8,18 @@ class DessertItem():
     def setters(self, name):
         self.name = name
 
+    def get_cost(self):
+        return self.price
+
+    def tax_percent(self):
+        return 7.25
+
+    def calculate_cost(self):
+        return self.get_cost() + self.get_cost() * self.tax_percent() / 100
+
+    def calculate_tax(self):
+        return self.get_cost() * self.tax_percent() / 100
+
 
 class Candy(DessertItem):
     def __init__(self, name, price=float, weight=float):
@@ -16,11 +28,14 @@ class Candy(DessertItem):
         self.weight = weight
 
     def getters(self):
-        return self.name, self.price, self.weight
+        return self.price, self.weight
 
     def setters(self, price, weight):
         self.price = price
         self.weight = weight
+
+    def calculate_cost(self):
+        return super().calculate_cost()
 
 
 class Cookie(DessertItem):
@@ -30,11 +45,14 @@ class Cookie(DessertItem):
         self.number = number
 
     def getters(self):
-        return self.name, self.price, self.number
+        return self.price, self.number
 
     def setters(self, price, number):
         self.price = price
         self.number = number
+
+    def calculate_cost(self):
+        return super().calculate_cost()
 
 
 class IceCream(DessertItem):
@@ -44,11 +62,14 @@ class IceCream(DessertItem):
         self.scoop = scoop
 
     def getters(self):
-        return self.name, self.price, self.scoop
+        return self.price, self.scoop
 
     def setters(self, price, flavor):
         self.price = price
         self.flavor = flavor
+
+    def calculate_cost(self):
+        return super().calculate_cost()
 
 
 class Sundae(IceCream):
@@ -58,11 +79,14 @@ class Sundae(IceCream):
         self.topping_price = topping_price
 
     def getters(self):
-        return self.name, self.topping, self.topping_price
+        return self.topping, self.topping_price
 
     def setters(self, topping, topping_price):
         self.topping = topping
         self.topping_price = topping_price
+
+    def calculate_cost(self):
+        return super().calculate_cost() + self.topping_price
 
 
 class Order():
@@ -93,15 +117,20 @@ class Order():
 
 def main():
     order = Order()
-    order.add(Candy("Candy Corn", 1.5, .25))
     order.add(Candy("Gummy Bears", .25, .35))
     order.add(Cookie("Chocolate Chip", 6, 3.99))
     order.add(IceCream("Pistachio", 2, .79))
     order.add(Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29))
     order.add(Cookie("Oatmeal Raisin", 2, 3.45))
+    order.add(IceCream("Chocolate", 3, .99))
+    order.add(Cookie("Chocolate Chip", 3, 3.99))
     for item in order.getOrderList():
-        print(item.getters()[0])
-    print("Total number of items in order:", (order.itemCount()))
+        print(
+            f"{item.name}: ${item.calculate_cost():.2f} [Tax:{item.calculate_tax():.2f}]")
+    print(
+        f"Order Subtotals: ${order.order_cost():.2f} [Tax:{order.order_tax():.2f}]")
+    print(f"Order Total: ${order.order_cost() + order.order_tax():.2f}")
+    print(f"Total items in the order: {order.itemCount()}")
 
 
 if __name__ == "__main__":
