19df7510d954e40a8bdeb6bcf69c70e0cd182691
dwk601 <10829539@uvu.edu>
2022-10-27 14:54:25 -0600

    finished payroll.py project

--- Dessert/payroll.py
+++ Dessert/payroll.py
@@ -18,132 +18,185 @@
 #4.Implement the Classification Hierarchy
 #5.Implement process_timecards
 #6.Implement process_receipts
-import os
+from abc import ABC, abstractmethod
+import os, os.path, shutil
 
 PAY_LOGFILE = 'paylog.txt'
+
 EMPLOYEES_FILE = 'employees.csv'
 TIMECARDS_FILE = 'timecards.csv'
 RECEIPTS_FILE = 'receipts.csv'
 
 employees = []
 
-class Employee:
-    def __init__(self, id, name, address, type):
-        self.id = id
-        self.name = name
-        self.address = address
-        self.type = type
-        self.classification = None
-
-    def make_salaried(self, salary):
-        self.classification = SalariedClassification(salary)
-
-    def make_hourly(self, hourly_rate):
-        self.classification = HourlyClassification(hourly_rate)
-
-    def make_commissioned(self, salary, commission_rate):
-        self.classification = CommissionedClassification(salary, commission_rate)
-
-    def issue_payment(self):
-        amount = self.classification.calculate_pay()
-        self.pay(amount)
-
-    def pay(self, amount):
-        with open(PAY_LOGFILE, 'a') as f:
-            f.write(f'Mailing {amount} to {self.name} at {self.address}')
-            
-            
-class Classification:
-    def __init__(self):
-        self.timecards = []
-        self.receipts = []
-
-    def add_timecard(self, hours):
-        self.timecards.append(hours)
-
-    def add_receipt(self, amount):
-        self.receipts.append(amount)
-
-    def calculate_pay(self):
-        raise NotImplementedError('Subclass must implement abstract method')
-    
-class SalariedClassification(Classification):
-    def __init__(self, salary):
-        super().__init__()
-        self.salary = salary
-
-    def calculate_pay(self):
-        return self.salary / 24
-    
-class HourlyClassification(Classification):
-    def __init__(self, hourly_rate):
-        super().__init__()
-        self.hourly_rate = hourly_rate
-
-    def calculate_pay(self):
-        total_hours = sum(self.timecards)
-        if total_hours > 8:
-            overtime = total_hours - 8
-            total_hours = 8 + overtime * 1.5
-        return total_hours * self.hourly_rate
-    
-class CommissionedClassification(Classification):
-    def __init__(self, salary, commission_rate):
-        super().__init__()
-        self.salary = salary
-        self.commission_rate = commission_rate
-
-    def calculate_pay(self):
-        total = self.salary / 24
-        total += sum(self.receipts) * self.commission_rate / 100
-        return total
-    
 def load_employees():
-    with open(EMPLOYEES_FILE) as f:
-        next(f)
-        for line in f:
-            id, name, address, type = line.strip().split(',')
-            employee = Employee(id, name, address, type)
-            if type == 'Salaried':
-                employee.make_salaried(3000)
-            elif type == 'Hourly':
-                employee.make_hourly(15)
-            elif type == 'Commissioned':
-                employee.make_commissioned(2500, 10)
-            employees.append(employee)
-            
-def find_employee_by_id(id):
-    for employee in employees:
-        if employee.id == id:
-            return employee
-        
+    '''
+    Reads "employees.csv, creates Employee objects and puts them in the list named employees.
+    '''
+    employee_file = open(EMPLOYEES_FILE)
+    for line in employee_file:
+        employee_data = line.split(",")
+        employee_object = Employee(employee_data[0], employee_data[1], employee_data[2], employee_data[3], employee_data[4], employee_data[5], employee_data[6], employee_data[7])
+        employees.append(employee_object)
+        if employee_data[7] == "3":
+            employee_object.make_hourly(employee_data[10])
+        elif employee_data[7] == "2":
+            employee_object.make_commissioned(employee_data[8], employee_data[9])
+        else:
+            employee_object.make_salaried(employee_data[8])
+
+def find_employee_by_id(emp_id_num):
+    '''
+    Searches the list of employees, finds the matching ID and returns the employee object.
+    '''
+    for emp in employees:
+        if emp.emp_id == emp_id_num:
+            return emp
+
 def process_timecards():
-    with open(TIMECARDS_FILE) as f:
-        next(f)
-        for line in f:
-            id, hours = line.strip().split(',')
-            employee = find_employee_by_id(id)
-            employee.classification.add_timecard(float(hours))
-            
+    '''
+    Reads the file "timecard.csv and add the hours to the corresponding employee.
+    '''
+    timecard_file = open(TIMECARDS_FILE)
+    for line in timecard_file:
+        emp_hrs = line.split(",")
+        for emp in employees:
+            if emp.emp_id == emp_hrs[0]:
+                for hr in emp_hrs[1:]:
+                    hr = float(hr)
+                    emp.classification.timecards.append(hr)
+
 def process_receipts():
-    with open(RECEIPTS_FILE) as f:
-        next(f)
-        for line in f:
-            id, amount = line.strip().split(',')
-            employee = find_employee_by_id(id)
-            employee.classification.add_receipt(float(amount))
-            
-def issue_payments():
-    for employee in employees:
-        employee.issue_payment()
-        
-def main():
+    '''
+    Reads the file "receipts.csv and adds the sales to the corresponding employee.
+    '''
+    receipt_file = open(RECEIPTS_FILE)
+    for line in receipt_file:
+        emp_sales = line.split(",")
+        for emp in employees:
+            if emp.emp_id == emp_sales[0]:
+                for sale in emp_sales[1:]:
+                    sale = float(sale)
+                    emp.classification.sales.append(sale)
+
+def run_payroll():
+    '''
+    Removes the old paylog.txt and creates new one to write out to.
+    '''
     if os.path.exists(PAY_LOGFILE):
         os.remove(PAY_LOGFILE)
-    load_employees()
-    process_timecards()
-    process_receipts()
-    issue_payments()
+    for emp in employees:
+        emp.issue_payment()
 
-if __name__ == '__main__':
-    main()
+
+class Employee():
+    '''
+    This class contains the Employee attributes: emp_id, first_name, last_name, address, city, state, zipcode, classification.
+    '''
+
+    def __init__(self, emp_id, first_name, last_name, address, city, state, zipcode, classification):
+        self.emp_id = emp_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.classification = classification
+
+    def make_hourly(self, hourly_rate):
+        '''
+        Classificies the employee as an hourly employee.
+        '''
+        self.classification = Hourly(hourly_rate)
+
+    def make_commissioned(self, salary, commission_percentage):
+        '''
+        Classifies the employee as a commissioned employee.
+        '''
+        self.classification = Commissioned(salary, commission_percentage)
+
+    def make_salaried(self, salary):
+        '''
+        Classifies the employee as a salaried employee.
+        '''
+        self.classification = Salaried(salary)
+
+    def issue_payment(self):
+        '''
+        Writes payment amount out to the file along with employee information.
+        '''
+        paylog_file = open(PAY_LOGFILE, "a")
+        payAmount = self.classification.compute_pay()
+        paylog_file.write(f"Mailing {payAmount} to {self.first_name} {self.last_name} at {self.address} {self.city} {self.state} {self.zipcode}\n")
+
+
+class Classification(ABC):
+    '''
+    An abstract base class for the three different types of employees: Hourly, Commissioned and Salaried.
+    '''
+
+    @abstractmethod
+    def compute_pay(self):
+        return 0
+
+class Hourly(Classification):
+    '''
+    The Hourly employee class computes the payment for hourly employees.
+    '''
+
+    def __init__(self, hourly_rate):
+        self.hourly_rate = hourly_rate
+        self.timecards = []
+
+    def compute_pay(self):
+        '''
+        Computes payment for employee
+        '''
+        total_pay = sum(self.timecards) * float(self.hourly_rate)
+        return round(total_pay, 2)
+
+    def add_timecard(self, hours):
+        '''
+        Adds hours to an employee
+        '''
+        self.timecards.append(hours)
+
+class Salaried(Classification):
+    '''
+    The Salaried employee class returns the salary for Salaried employees.
+    '''
+
+    def __init__(self, salary):
+        self.salary = salary
+        
+    def compute_pay(self):
+        '''
+        Returns the salary
+        '''
+        return self.salary
+
+class Commissioned(Salaried):
+    '''
+    The Commissioned employee class returns the salary plus commissions for employee.
+    '''
+
+    def __init__(self, salary, commission_percentage):
+        self.salary = salary
+        self.commission_percentage = commission_percentage
+        self.sales = []
+
+    def compute_pay(self):
+        '''
+        Computes the payment for Commissioned employees.
+        '''
+        com_percent = float(self.commission_percentage) * 0.01
+        total_sales = sum(self.sales)
+        total_pay = float(self.salary) + (total_sales * com_percent)
+        return round(total_pay, 2)
+
+    def add_receipt(self, amount):
+        '''
+        Adds receipt to a Commissioned employee.
+        '''
+        self.sales.append(amount)
