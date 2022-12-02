6f2847c3bc4500f03994182dd0dd74d4b9c8d09f
dwk601 <108056780+dwk601@users.noreply.github.com>
2022-12-02 16:00:14 -0700

    Human Pyramids CS1410 Project

---
+++ linux_bash.py
@@ -0,0 +1,17 @@
+# create folder using mkdir command
+# Classes
+#     CS1410
+#     Files
+#     Projects
+# MATH1100
+#     Homework
+# Notes
+#     CS1410
+#     MATH1100
+
+echo "Creating directories..."
+mkdir -p CS1410/Files/Projects
+mkdir -p MATH1100/Homework
+mkdir -p Notes/CS1410
+mkdir -p Notes/MATH1100
+echo "Done!"
